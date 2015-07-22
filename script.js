97c26705f164fae79a39b0b331a656cf668f8839
bruntrd <bruntrd@gmail.com>
2015-07-22 09:13:20 -0500

    Second Commit

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ var soldBananas=0;
 var soldPears=0;
 
 var avgApples=0;
+var avgOranges=0;
+var avgBananas=0;
+var avgPears=0;
+
+var totalApples=0;
+var totalOranges=0;
+var totalBananas=0;
+var totalPears=0;
 
 var apples = {name:"apples", price:randomNumber(1, 9.5)};
 var oranges = {name:"oranges", price:randomNumber(1, 9.5)};
@@ -44,7 +52,7 @@ console.log(apples);
 	 		}else if(market[i].price==0.5){
 	 			market[i].price+=0.5;
 	 		}else{
-	 			console.log(market[i].price)
+	 			
 	 			var random=randomNumber(1,2);
 	 			if(random == 1){
 	 				market[i].price-=0.5;
@@ -61,21 +69,22 @@ console.log(apples);
 	$("#orangePrice").text(oranges.price);
 	$("#bananaPrice").text(bananas.price);
 	$("#pearPrice").text(pears.price);
-	
+	  console.log(apples.price);
 		
-	}, 1500);
+	}, 500);
 
 	 $("#buyApple").on('click', function(){
 	  	if(cash-apples.price>=0){
 	  	cash-=apples.price;
 	  	numApples++;
+	  	totalApples++
 	  	spentApples += apples.price;
 	  	console.log("button result "+cash+" "+numApples);
-	  	$("#applesbought").text(numApples);
+	  	$("#applesbought").text(totalApples);
 	  	$("#wallet").text(cash);
 	  	$("#appleAvg").text((spentApples/numApples).toFixed(2));
 	  }else{
-	  	alert("No money");
+	  	alert("No money!");
 	  }
 	  });
 
@@ -88,7 +97,7 @@ console.log(apples);
 	  	$("#wallet").text(cash);
 	  	$("#orangeAvg").text((spentOranges/numOranges).toFixed(2));
 	  	 }else{
-	  	alert("No money");
+	  	alert("No money!");
 	  	}
 	  });
 
@@ -101,7 +110,7 @@ console.log(apples);
 	  	$("#wallet").text(cash);
 	  	$("#bananaAvg").text((spentBananas/numBanana).toFixed(2));
 	  	 }else{
-	  	alert("No money");
+	  	alert("No money!");
 	  	}
 	  });
 
@@ -114,35 +123,67 @@ console.log(apples);
 	  	$("#wallet").text(cash);
 	  	$("#pearAvg").text((spentPears/numPears).toFixed(2));
 	  	 }else{
-	  	alert("No money");
+	  	alert("No money!");
 	  	}
 	  });
 	 $("#sellApple").on('click',function(){
+	 	if (numApples>0){
 	 	cash+=apples.price;
-	 	numApples--;
+	 	totalApples--;
 	 	soldApples++;
 	 	avgApples+=apples.price;
 	 	$("#applesbought").text(numApples);
 	 	$("#wallet").text(cash);
-	 	$("#appleSales").text(avgApples/soldApples);
+	 	$("#appleSales").text((avgApples/soldApples).toFixed(2));
+	 }else{
+	 	alert("You got no apples!");
+	 }
+	});
+	  $("#sellOrange").on('click',function(){
+	 	if (numOranges>0){
+	 	cash+=oranges.price;
+	 	numOranges--;
+	 	soldOranges++;
+	 	avgOranges+=oranges.price;
+	 	$("#orangesbought").text(numOranges);
+	 	$("#wallet").text(cash);
+	 	$("#orangeSales").text((avgOranges/soldOranges).toFixed(2));
+	 }else{
+	 	alert("You got no Oranges!");
+	 }
+	});
+	  $("#sellBanana").on('click',function(){
+	 	if (numBanana>0){
+	 	cash+=bananas.price;
+	 	numBanana--;
+	 	soldBananas++;
+	 	avgBananas+=bananas.price;
+	 	$("#bananasbought").text(numBanana);
+	 	$("#wallet").text(cash);
+	 	$("#bananaSales").text((avgBananas/soldBananas).toFixed(2));
+	 }else{
+	 	alert("You got no Bananas!");
+	 }
+	});
+	  $("#sellPear").on('click',function(){
+	 	if (numPears>0){
+	 	cash+=pears.price;
+	 	numPears--;
+	 	soldPears++;
+	 	avgPears+=pears.price;
+	 	$("#pearsbought").text(numPears);
+	 	$("#wallet").text(cash);
+	 	$("#pearSales").text((avgPears/soldPears).toFixed(2));
+	 }else{
+	 	alert("You got no Pears!");
+	 }
 
 	 });
 
 
 });
 
-console.log(priceChange(6));
-function priceChange (fruitPrice) {
-	 		var random=randomNumber(1,2);
-	 		if(random == 1){
-	 			fruitPrice -=0.5;
-	 			return fruitPrice;
-	 		}else {
-	 			fruitPrice +=0.5;
-	 			return fruitPrice;
-	 		}
 
-}
 
 
 
